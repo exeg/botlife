d7eea1aa5229d7ea6a324377c84d9dd6b671f8bc
Pobepto <roobertdantes@gmail.com>
2018-08-09 10:56:33 +0300

    feat: update bot

---
+++ templates/telegramTemplate.js
@@ -0,0 +1,20 @@
+exports.telegramTemplate = {
+  message_id: 0,
+  from: {
+    id: 0,
+    is_bot: false,
+    first_name: "",
+    last_name: "",
+    username: "",
+    language_code: "en-us"
+  },
+  chat: {
+    id: 0,
+    first_name: "",
+    last_name: "",
+    username: "",
+    type: "private"
+  },
+  date: 0,
+  text: ""
+}
